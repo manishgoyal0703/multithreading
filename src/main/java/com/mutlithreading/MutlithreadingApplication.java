f4888ddd6d78f64af421b2179a8ae5a0ac234e38
manish goyal <manishgoyal0703@gmail.com>
2023-09-04 18:10:05 +0530

    multithreading in java

---
+++ src/main/java/com/mutlithreading/MutlithreadingApplication.java
@@ -0,0 +1,13 @@
+package com.mutlithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutlithreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutlithreadingApplication.class, args);
+	}
+
+}
